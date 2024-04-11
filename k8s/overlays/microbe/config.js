e284535ad86d9f5c1babde2780a43fc48ad59ea3
Amnon Khen <amnon@ebi.ac.uk>
2024-04-11 13:34:55 +0100

    dc-33 microbe demo files and deployment

--- k8s/overlays/microbe/config.js
+++ k8s/overlays/microbe/config.js
@@ -1,7 +1,7 @@
 window.appConfig = {
     "ENVIRONMENT": "microbe",
     "basename": "/catalogue/microbe",
-    "REST_ENDPOINT_URL": '/biosamples/samples?filter=attr%3Aproject+name%3AMICROBE&filter=attr%3Acenter&size=200',
+    "REST_ENDPOINT_URL": '/biosamples/samples?filter=attr%3Aproject+name%3AMICROBE&filter=attr%3Acenter&filter=attr%3Apreservation%20temperature&filter=attr%3Atest%3Ayes&size=200',
     "SCHEMA_ENDPOINT_URL": '/biosamples/schemas/core/sample.json',
     "RESOURCE_JSON_PATH": '_embedded.samples',
     "GRID_CONFIG": [
@@ -15,9 +15,6 @@ window.appConfig = {
         {
             "name": "timepoint",
             valueGetter: params => params.data.characteristics?.["time point"][0]?.text
-        },
-        {
-            "name": "release",
         },
         {
             "name": "accession",
@@ -48,10 +45,6 @@ window.appConfig = {
             valueGetter: params => params.data.characteristics?.["soil type"]?.[0].text
         },
         {
-            "name": "analysisDate",
-            valueGetter: params => params.data.characteristics?.["analysis date"]?.[0].text
-        },
-        {
             "name": "preservationTemperature",
             valueGetter: params => params.data.characteristics?.["preservation temperature"]?.[0].text
         },
@@ -74,16 +67,12 @@ window.appConfig = {
         {
             "name": "project name",
             "valueGetter": params => params.data.characteristics?.["project name"]?.[0]?.text
-        },
-        {
-            "name": "local environmental context",
-            "valueGetter": params => params.data.characteristics?.["local environmental context"]?.[0]?.text
         }
     ],
 
     FILTER_FIELDS: [
         {
-            field: "center",
+            field: "soilType",
             type: "checkbox",
             data_type: "string"
         },
@@ -93,12 +82,13 @@ window.appConfig = {
             data_type: "string"
         },
         {
-            field: "preservationTemperature",
+            field: "cultivation",
             type: "checkbox",
             data_type: "string"
         },
+        ,
         {
-            field: "freezingMethod",
+            field: "timepoint",
             type: "checkbox",
             data_type: "string"
         },
@@ -108,7 +98,7 @@ window.appConfig = {
             data_type: "string"
         },
         {
-            field: "timepoint",
+            field: "freezingMethod",
             type: "checkbox",
             data_type: "string"
         }
