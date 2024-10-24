da3a131c834e9f821e7341af546f92082afb136d
Amnon Khen <amnon@ebi.ac.uk>
2024-10-24 15:22:59 +0100

    mc-2 microbe catalogue k8s deployment and ci/cd, helm support

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ const SampleList: React.FC = (props) => (
     <List {...props}>
         <Datagrid rowClick="show">
             <TextField source="characteristics.organism[0].text" label="Organism"/>
-            <TextField source="characteristics.soil_type[0].text" label="Soil Type"/>
+            <TextField source="characteristics.soil type[0].text" label="Soil Type"/>
             <TextField source="characteristics.biome[0].text" label="Biome"/>
             <TextField source="accession" />
             <TextField source="characteristics.SRA accession[0].text" label="SRA Accession" />
+            <TextField source="characteristics.environmental medium[0].text" label="Environmental Medium" />
+            <TextField source="characteristics.geographic location (country and/or sea)[0].text" label="Country/Sea" />
         </Datagrid>
     </List>
 );
