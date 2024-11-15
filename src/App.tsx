658c1c77f112d74339f2be28fa988e075931af0f
Amnon Khen <amnon@ebi.ac.uk>
2024-11-15 11:17:05 +0000

    mc-2: field list from Miro board

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ import biosamplesDataProvider from './biosamplesDataProvider';
 const SampleList: React.FC = (props) => (
     <List {...props}>
         <Datagrid rowClick="show">
-            <TextField source="characteristics.organism[0].text" label="Organism"/>
-            <TextField source="characteristics.soil type[0].text" label="Soil Type"/>
-            <TextField source="characteristics.biome[0].text" label="Biome"/>
+            <TextField source="characteristics.center[0].text" label="Centre"/>
+            <TextField source="characteristics.time point[0].text" label="Time Point"/>
+            <TextField source="characteristics.cryoprotectant[0].text" label="Cryoprotectant" />
+            <TextField source="characteristics.freezing method[0].text" label="Freezing Method" />
+            <TextField source="characteristics.preservation temperature[0].text" label="Preservation Temperature" />
+            <TextField source="characteristics.targets[0].text" label="Targets" />
             <TextField source="accession" />
-            <TextField source="characteristics.SRA accession[0].text" label="SRA Accession" />
-            <TextField source="characteristics.environmental medium[0].text" label="Environmental Medium" />
-            <TextField source="characteristics.geographic location (country and/or sea)[0].text" label="Country/Sea" />
         </Datagrid>
     </List>
 );
