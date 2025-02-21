da9f393483c1d122626ce70da635b5ecb58e6a1d
ESapenaVentura <sapenaventura@gmail.com>
2025-02-21 16:42:34 +0000

    Changed side bar (left, buttons); More fields; DataGridConfigurable

--- src/SampleList.tsx
+++ src/SampleList.tsx
@@ -1,7 +1,7 @@
-import {Card, CardContent} from "@mui/material";
+import {Card, CardContent, CardHeader} from "@mui/material";
 import React from "react";
-import {Button, Datagrid, FilterButton, List, TextField, TopToolbar, useListContext, WrapperField} from "react-admin";
-import {DynamicFilterList} from "./DynamicFilterList";
+import {SelectColumnsButton, Button, ExportButton, DatagridConfigurable, List, TextField, TopToolbar, useListContext, WrapperField, UrlField, FunctionField, SelectInput, FilterLiveSearch} from "react-admin";
+import {FilterDropdown} from "./DynamicFilterList";
 
 const CharacteristicField = props => {
     const {source} = props;
@@ -12,8 +12,30 @@ const CharacteristicField = props => {
     </WrapperField>
 };
 
+const AccessionRender = record => (
+    <a target="_blank" href={`https://www.ebi.ac.uk/biosamples/samples/${record.accession}`}>
+        {record.accession}
+    </a>
+    );
+
+
+const BioSamplesAccessionField = (props) => {
+    const {source} = props;
+    return <WrapperField>
+        <FunctionField
+        label="Biosamples entry"
+    render={AccessionRender}
+/>
+    </WrapperField>
+};
+
+
 const ClearFilterButton = () => {
     const {setFilters} = useListContext();
+    const filters = useListContext().filterValues;
+    if (Object.keys(filters).length === 0) {
+        return null;
+    }
 
     const clearFilters = () => {
         setFilters({}, {}); // Pass empty objects to clear all filters
@@ -25,41 +47,45 @@ const filters = [
     // <SearchInput source="q" alwaysOn />,
 ];
 const ListActions = () => (
-    <TopToolbar>
-        <FilterButton/>
-        {/*<SelectColumnsButton />*/}
-        <ClearFilterButton/>
-        {/*<ExportButton/>*/}
+    <TopToolbar sx={{ order: -1 }}>
+        <SelectColumnsButton />
     </TopToolbar>
 );
 export const SampleFilterSidebar = (props) => {
     return (
-        <Card>
+        <Card sx={{ order: -1, mr: 2, mt: 9, width: 300 }} variant="outlined">
+            <CardHeader title="Filters" subheader="Please select from the dropdown menus below to filter the samples"/>
             <CardContent>
-                <DynamicFilterList source={'organism'}/>
-                <DynamicFilterList source={'center'}/>
-                {/* TODO: freezing is hard coded becuase it is not available in the
-                          facets resource
-                */}
-                <DynamicFilterList source={'freezing method'}/>
+                <FilterDropdown source={'environmental medium'}/>
+                <FilterDropdown source={'time point'}/>
+                <FilterDropdown source={'cryoprotectant'}/>
+                <FilterDropdown source={'freezing method'}/>
+                <FilterDropdown source={'preservation method'}/>
+                <FilterDropdown source={'targets'}/>
+                <br />
+                <ClearFilterButton/>
             </CardContent>
         </Card>
     )
 };
+
+
 export const SampleList: React.FC = (props) => (
     <List {...props}
           actions={<ListActions/>}
           aside={<SampleFilterSidebar/>}
           filters={filters}
+
     >
-        <Datagrid rowClick="show">
+        <DatagridConfigurable rowClick="show">
+            <TextField source="name"/>
+            <BioSamplesAccessionField source="accession" />
             <CharacteristicField source="center"/>
             <CharacteristicField source="time point"/>
             <CharacteristicField source="cryoprotectant"/>
             <CharacteristicField source="freezing method"/>
             <CharacteristicField source="preservation temperature"/>
             <CharacteristicField source="targets"/>
-            <TextField source="accession"/>
-        </Datagrid>
+        </DatagridConfigurable>
     </List>
 );
