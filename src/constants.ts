4b4a04a8dfd518f45d801ccb7c19934367a13d13
ESapenaVentura <sapenaventura@gmail.com>
2025-02-21 16:07:34 +0000

    Added soil, water and seed filters

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,4 @@
-export const defaultFilter = ['attr:project+name:MICROBE', 'attr%3Acenter'];
+export const defaultFilter = ['attr:project+name:MICROBE']
+export const soilFilter = ['attr:project+name:MICROBE', 'attr:organism:soil+metagenome']
+export const waterFilter = ['attr:project+name:MICROBE', 'attr:organism:marine+metagenome']
+export const seedFilter = ['attr:project+name:MICROBE', 'attr:organism:seed+metagenome']
