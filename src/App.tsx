14c7e5e008862cadb63a1829d21670c81d6b1c7c
Amnon Khen <amnon@ebi.ac.uk>
2025-03-11 15:38:14 +0000

    mc-2: more comprehensive defaults for time point, remove text search

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import {QueryClient} from '@tanstack/react-query';
+import {ImageFieldClasses} from "ra-ui-materialui/src/field/ImageField";
 import React from 'react';
-import {Admin, Resource} from 'react-admin';
+import {Admin, AppBar, Layout, Resource, TitlePortal} from 'react-admin';
 import {customDataProvider} from "./delegatingDataProvider";
 import {SampleList} from "./SampleList";
+import {Box} from '@mui/material';
+import logo from './assets/Microbe_RGB.png';
 
+
+export const MyLayout = ({ children }) => (
+    <Layout appBar={MyAppBar}>
+        {children}
+    </Layout>
+);
+
+export const MyAppBar = () => (
+    <AppBar color="primary">
+        <TitlePortal />
+        <Box sx={{ flex: "1",  }}>
+            <img
+                alt={'Microbe Logo'}
+                title={'Microbe Logo'}
+                height={100}
+                src={logo}
+            />
+        </Box>
+    </AppBar>
+);
 
 const App: React.FC = () => {
     const queryClient = new QueryClient({
@@ -16,7 +39,9 @@ const App: React.FC = () => {
     return (
 
         <Admin dataProvider={customDataProvider}
-               queryClient={queryClient}>
+               queryClient={queryClient}
+               layout={MyLayout}
+        >
             <Resource name="samples" list={SampleList}/>
         </Admin>
     );
