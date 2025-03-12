ff434e70ebacec1a5da054cefd794a2bd466e853
Amnon Khen <amnon@ebi.ac.uk>
2025-03-12 08:37:40 +0000

    mc-2: wip custom title bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,73 @@
+import {Box} from '@mui/material';
 import {QueryClient} from '@tanstack/react-query';
-import {ImageFieldClasses} from "ra-ui-materialui/src/field/ImageField";
 import React from 'react';
-import {Admin, AppBar, Layout, Resource, TitlePortal} from 'react-admin';
+import {Admin, AppBar, Layout, Menu, Resource, Sidebar, TitlePortal, useCreatePath} from 'react-admin';
+import logo from './assets/Microbe_RGB.png';
 import {customDataProvider} from "./delegatingDataProvider";
 import {SampleList} from "./SampleList";
-import {Box} from '@mui/material';
-import logo from './assets/Microbe_RGB.png';
+import {Terrain, Tsunami, ScatterPlot, Biotech} from '@mui/icons-material'
+import {stringify} from 'query-string';
 
 
-export const MyLayout = ({ children }) => (
-    <Layout appBar={MyAppBar}>
+export const MyLayout = ({children}) => (
+    <Layout appBar={MyAppBar}
+            menu={MyMenu}
+            appBarAlwaysOn={true}
+            sidebar={MySidebar}>
         {children}
     </Layout>
 );
 
 export const MyAppBar = () => (
     <AppBar color="primary">
-        <TitlePortal />
-        <Box sx={{ flex: "1",  }}>
-            <img
-                alt={'Microbe Logo'}
-                title={'Microbe Logo'}
-                height={100}
-                src={logo}
-            />
-        </Box>
+        <TitlePortal/>
+        {/*<Box sx={{flex: "1"}}/>*/}
+        {/*<Box sx={{flex: "1"}}>*/}
+        {/*    <img*/}
+        {/*        alt={'Microbe Logo'}*/}
+        {/*        title={'Microbe Logo'}*/}
+        {/*        height={80}*/}
+        {/*        src={logo}*/}
+        {/*    />*/}
+        {/*</Box>*/}
+        {/*<Box sx={{flex: "1"}}/>*/}
     </AppBar>
 );
 
+// menu:   http://localhost:5173/microbe#/samples?filter=attr%3Aorganism%3Asoil%20metagenome
+// filter: http://localhost:5173/microbe#/samples?filter=%7B%22attr%3Aorganism%22%3A%22soil%20metagenome%22%7D&order=ASC&page=1&perPage=10&sort=id
+const FilteredResourceLink = (props: { resource, filter, icon }) => {
+    const createPath = useCreatePath();
+    const filterQueryString = JSON.stringify({filter:props.filter});
+    const filterPath = createPath({resource: props.resource, type: "list"})+'?'+filterQueryString;
+    return <Menu.Item
+        to={filterPath}
+        leftIcon={props.icon}
+    />;
+};
+
+export const MyMenu = () => {
+    return (
+        <Menu>
+            {/*<Menu.DashboardItem/>*/}
+            <Menu.Item to="/samples" primaryText="Samples" leftIcon={<Biotech/>}/>
+            {/*<FilteredResourceLink resource="samples" filter={{filter: "attr:organism:soil metagenome"}} icon={<Terrain/>}/>*/}
+            {/*<FilteredResourceLink resource="samples" filter={{filter: "attr:organism:marine metagenome"}} icon={<Tsunami/>}/>*/}
+            {/*<FilteredResourceLink resource="samples" filter={{filter: "attr:organism:seed metagenome"}} icon={<ScatterPlot/>}/>*/}
+        </Menu>
+    );
+};
+
+const MySidebar = (props) => (
+    <Sidebar
+        sx={{
+            "& .RaSidebar-drawerPaper": {
+                backgroundColor: "red",
+            },
+        }}
+        {...props}
+    />
+);
 const App: React.FC = () => {
     const queryClient = new QueryClient({
         defaultOptions: {
