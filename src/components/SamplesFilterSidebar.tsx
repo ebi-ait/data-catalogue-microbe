827cb5ab86236ae2d7192fcd7cdf55bc69ef64a8
Amnon Khen <amnon@ebi.ac.uk>
2025-03-26 13:54:33 +0000

    mc-4: reposition logo

--- src/components/SamplesFilterSidebar.tsx
+++ src/components/SamplesFilterSidebar.tsx
@@ -1,6 +1,7 @@
-import {Card, CardContent} from "@mui/material";
+import {Box, Card, CardContent} from "@mui/material";
 import React from "react";
 import {SavedQueriesList} from "react-admin";
+import logo from "../assets/Microbe_RGB.png";
 import {ClearFilterButton} from "./ClearFilterButton";
 import {DynamicFilterList} from "./DynamicFilterList";
 
@@ -8,6 +9,14 @@ export const SamplesFilterSidebar = (props) => {
     return (
         <Card sx={{order: -1, mr: 2, mt: 8, width: '25vw'}}>
             <CardContent>
+                <Box sx={{flex: "1"}}>
+                    <img
+                        alt={'Microbe Logo'}
+                        title={'Microbe Logo'}
+                        height={50}
+                        src={logo}
+                    />
+                </Box>
                 <SavedQueriesList />
                 <ClearFilterButton/>
                 <DynamicFilterList source={'organism'}/>
--- src/layout.tsx
+++ src/layout.tsx
@@ -2,30 +2,19 @@ import {Biotech, ScatterPlot, Terrain, Tsunami} from "@mui/icons-material";
 import {stringify} from "query-string";
 import React from "react";
 import {AppBar, Layout, Menu, MenuItemLink, Sidebar, TitlePortal} from "react-admin";
-import {Box} from '@mui/material';
-import logo from './assets/Microbe_RGB.png';
 
 export const MyLayout = ({children}) => (
-    <Layout appBar={MyAppBar}
-            menu={MyMenu}
-            appBarAlwaysOn={true}
-            sidebar={MySidebar}>
+    <Layout
+        // appBar={MyAppBar}
+        menu={MyMenu}
+        appBarAlwaysOn={true}
+        sidebar={MySidebar}>
         {children}
     </Layout>
 );
 export const MyAppBar = () => (
     <AppBar color="primary">
         <TitlePortal/>
-        <Box sx={{flex: "1"}}/>
-        <Box sx={{flex: "1"}}>
-            <img
-                alt={'Microbe Logo'}
-                title={'Microbe Logo'}
-                height={50}
-                src={logo}
-            />
-        </Box>
-        <Box sx={{flex: "1"}}/>
     </AppBar>
 );
 const FilteredResourceLink = (props: { resource, filter, icon, primaryText }) => {
@@ -40,7 +29,7 @@ const FilteredResourceLink = (props: { resource, filter, icon, primaryText }) =>
         leftIcon={props.icon}
     />;
 };
-const FieldFilterLink = (props:{resource, field, value, icon, title}) => {
+const FieldFilterLink = (props: { resource, field, value, icon, title }) => {
     return <FilteredResourceLink resource={props.resource}
                                  filter={{[props.field]: props.value}}
                                  primaryText={props.title ?? props.value}
