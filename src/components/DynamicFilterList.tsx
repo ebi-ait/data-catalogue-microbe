5a7191a3dd95f0fd124db58a3624c998fbce9459
Amnon Khen <amnon@ebi.ac.uk>
2025-03-27 17:09:44 +0000

    mc-6: update facets according to filter

--- src/components/DynamicFilterList.tsx
+++ src/components/DynamicFilterList.tsx
@@ -1,8 +1,10 @@
+import CancelIcon from "@mui/icons-material/CancelOutlined";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {Accordion, AccordionDetails, AccordionSummary, Typography, Chip, Stack} from '@mui/material';
+import {Accordion, AccordionDetails, AccordionSummary, Chip, Stack, Typography} from '@mui/material';
 import {useListFilterContext} from "ra-core";
 import React, {useState} from "react";
 import {FilterList, FilterListItem, Loading, useGetList, useTranslate} from "react-admin";
+
 
 interface DynamicFilterListProps {
     source: string;
@@ -44,7 +46,29 @@ export const DynamicFilterList = ({source, label, defaultValues}: DynamicFilterL
     const [expanded, setExpanded] = useState(false);
     const translate = useTranslate();
 
-    const { filterValues } = useListFilterContext();
+    const { filterValues, setFilters } = useListFilterContext();
+
+    const toggleFilter = (value, filters) => {
+        const isSelected = filters[Object.keys(value)]==Object.values(value);
+
+        if (isSelected) {
+            const keysToRemove = Object.keys(value);
+            return Object.keys(filters).reduce(
+                (acc, key) =>
+                    keysToRemove.includes(key)
+                        ? acc
+                        : { ...acc, [key]: filters[key] },
+                {}
+            );
+        }
+
+        return { ...filters, ...value };
+    };
+
+    const handleDeleteFilter = (value) => {
+        setFilters(toggleFilter({ [`attr:${source}`] : value }, filterValues));
+    };
+
 
     if (isPending) {
         return <Loading loadingSecondary={`Loading ${source}`}
@@ -60,7 +84,14 @@ export const DynamicFilterList = ({source, label, defaultValues}: DynamicFilterL
             <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
                 <Stack>
                     <Typography component="span">{translate(label || source)}</Typography>
-                    {filterValues.hasOwnProperty('attr:'+source)?<Chip label={filterValues['attr:'+source]}/>:''}
+                    {(filterValues.hasOwnProperty('attr:'+source) && !expanded)
+                        ?<Chip label={filterValues['attr:'+source]}
+                               onDelete={event => {
+                                   event.stopPropagation();
+                                   handleDeleteFilter(filterValues['attr:'+source]);
+                               }}
+                                deleteIcon={ <CancelIcon />}/>
+                        :''}
                 </Stack>
             </AccordionSummary>
             {/*TODO: move to a css file */}
@@ -79,14 +110,16 @@ export const DynamicFilterList = ({source, label, defaultValues}: DynamicFilterL
             }}>
                 <FilterList source={source}
                             label="" icon="">
-                    {facetValues.sort((a, b) => a.label.localeCompare(b.label))
+                    {facetValues
+                        .sort((a, b) => a.label.localeCompare(b.label))
                         .map(value => {
                             const filterItemValue = {[`attr:${source}`]: value.label};
                             return (
                                 <FilterListItem
                                     key={value.label}
-                                    label={translate(value.label)}
+                                    label={`${translate(value.label)} (${value.count})`}
                                     value={filterItemValue}
+                                    toggleFilter={toggleFilter}
                                 />
                             );
                         })}
