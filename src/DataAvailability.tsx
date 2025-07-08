c88fdc0d39ee33268fef42314ef5ef4204482fae
Amnon Khen <amnon@ebi.ac.uk>
2025-07-08 16:27:04 +0100

    mc-8: data status column: loading icon

--- src/DataAvailability.tsx
+++ src/DataAvailability.tsx
@@ -1,26 +1,21 @@
 import {useFieldValue} from "ra-core";
 import React from "react";
-import {useDataProvider, useRecordContext} from "react-admin";
+import {Loading, useGetOne, useRecordContext} from "react-admin";
 
 export const DataAvailability = (props: any) => {
     const value = useFieldValue(props);
     const record = useRecordContext();
-    const dataProvider = useDataProvider();
-    const [datahubRecord, setDatahubRecord] = React.useState<any | null>(null);
+    const {data, isPending} = useGetOne('ena', {id: record.accession});
 
-    React.useEffect(() => {
-        if (record?.accession) {
-            dataProvider.getOne('ena', {id: record.accession})
-                .then(response => setDatahubRecord(response.data))
-                .catch(() => setDatahubRecord(null));
-        }
-    }, [dataProvider, record?.accession]);
-
+    if (isPending) {
+        return <Loading loadingSecondary={`Loading`}
+                        loadingPrimary={''}/>;
+    }
     if (value) {
         return <>Public</>;
-    } else if (datahubRecord) {
+    } else if (data) {
         return <>Private</>;
     } else {
-        return <>Not Sequenced Yet</>;
+        return <>Not Sequenced</>;
     }
-};
+}
